454d43438c3bb4e35587327cd847c2ef189d12e2
will <will.eizlini@gmail.com>
2021-07-14 03:14:52 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./public"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      /*Choose only one of the following two: if you're using
+      plain CSS, use the first one, and if you're using a
+      preprocessor, in this case SASS, use the second one*/
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./public"),
+    hot: true,
+    open: true,
+  },
+};
