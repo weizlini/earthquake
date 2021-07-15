56ad509c518bd12e6ebed1a6e0772436c07c925b
will <will.eizlini@gmail.com>
2021-07-15 10:13:47 -0400

    profile view

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSiteData } from "../contexts/DataContext";
+import "./Profile.css";
 const Profile = () => {
-  return <div>My Profile</div>;
+  const { data } = useSiteData();
+  const [profile, setProfile] = useState();
+  useEffect(() => {
+    if (data) {
+      setProfile(data.profile);
+    }
+  }, [data]);
+  return (
+    <>
+      {profile && (
+        <div className="profile-view">
+          <img src={profile.avatarImage} className="avatar" />
+          <table>
+            <tr>
+              <th>First Name</th>
+              <td>{profile.firstName}</td>
+            </tr>
+            <tr>
+              <th>Last Name</th>
+              <td>{profile.lastName}</td>
+            </tr>
+            <tr>
+              <th>Phone</th>
+              <td>{profile.phone}</td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td>{profile.email}</td>
+            </tr>
+            <tr>
+              <th>Bio</th>
+              <td>{profile.bio}</td>
+            </tr>
+          </table>
+        </div>
+      )}
+    </>
+  );
 };
 export default Profile;
